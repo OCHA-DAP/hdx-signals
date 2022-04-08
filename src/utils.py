e97e90e701b9e78188c302e6495ebe80e2d9fcb1
turnerm <turnermon@gmail.com>
2022-04-08 11:48:27 +0200

    Starting anom detection

--- src/utils.py
+++ src/utils.py
@@ -6,4 +6,13 @@ from src import constants
 
 
 def get_idmc_data(filepath_prefix=Path("..")):
-    return pd.read_csv(filepath_prefix / constants.idmc_output_filename)
+    df_idmc = pd.read_csv(filepath_prefix / constants.idmc_output_filename)
+    for column in [
+        "displacement_date",
+        "displacement_start_date",
+        "displacement_end_date",
+        "event_start_date",
+        "event_end_date",
+    ]:
+        df_idmc[column] = pd.to_datetime(df_idmc[column])
+    return df_idmc
