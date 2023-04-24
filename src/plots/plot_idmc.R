e555d128e387e21de5517ff36d59c8f475825a85
Seth Caldwell <caldwellst@gmail.com>
2023-04-24 09:14:33 +0100

    Update IDMC plot

--- src/plots/plot_idmc.R
+++ src/plots/plot_idmc.R
@@ -23,6 +23,23 @@ plot_idmc <- function(
       iso3 == !!iso3
     )
 
+  # generate flagging times for plotting
+  df_flag_times <- df_plot %>%
+    mutate(
+      flag_group = cumsum(!flag_any)
+    ) %>%
+    filter(
+      flag_any
+    ) %>%
+    group_by(
+      flag_group
+    ) %>%
+    summarize(
+      flag_start = min(date),
+      flag_end = max(date),
+      .groups = "drop"
+    )
+
   df_plot %>%
     mutate(
       year = lubridate::year(date),
@@ -37,14 +54,27 @@ plot_idmc <- function(
       displacement = sum(displacement_daily),
       .groups = "drop"
     ) %>%
-    ggplot(
+    ggplot() +
+    geom_rect(
+      data = df_flag_times,
+      mapping = aes(
+        xmin = flag_start,
+        xmax = flag_end
+      ),
+      ymin = 0,
+      ymax = Inf,
+      fill = hdx_hex("tomato-ultra-light")
+    ) +
+    geom_line(
       aes(
         x = date,
         y = displacement
       )
     ) +
-    geom_line() +
     expand_limits(y = 0) +
+    scale_y_continuous(
+      labels = scales::label_comma()
+    ) +
     scale_x_date(
       date_breaks = "6 months",
       labels = scales::label_date_short()
@@ -57,6 +87,7 @@ plot_idmc <- function(
       y = "Displacement (weekly)",
       color = "",
       title = "Historical weekly displacement",
+      subtitle = "Alerts highlighted in light red",
       caption = "Data from the IDMC, http://www.internal-displacement.org"
     )
 }
