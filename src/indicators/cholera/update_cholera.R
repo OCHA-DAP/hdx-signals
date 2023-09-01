d038161eec35692994609672f354422ed2124b40
Seth Caldwell <caldwellst@gmail.com>
2023-09-01 10:21:00 +0100

    Update workflows

---
+++ src/indicators/cholera/update_cholera.R
@@ -0,0 +1,176 @@
+# external packages
+box::use(dplyr)
+box::use(readr)
+box::use(lubridate)
+box::use(janitor)
+box::use(stringr)
+box::use(countrycode)
+box::use(zoo)
+box::use(tidyr)
+box::use(utils)
+box::use(scales)
+
+# internal utilities
+# first set the root search path for utilities
+box::use(gs = ../../utils/google_sheets)
+box::use(../../utils/get_country_names[get_country_names])
+box::use(../../utils/format_date[format_date])
+
+# alert helper function
+# only generate alert when boundary crossed from below
+lim_alert <- function(x, lim) {
+  x >= lim & dplyr$lag(x) < lim
+}
+
+#################
+#### CHOLERA ####
+#################
+
+f <- tempfile(fileext = ".csv")
+download.file(
+  url = Sys.getenv("CERF_CHOLERA_DATA"),
+  destfile = f
+)
+
+# raw data
+df_cholera_raw <- readr$read_csv(
+  f,
+  col_types = readr$cols(.default = "c")
+)
+
+# wrangled data
+df_cholera_wrangled <- df_cholera_raw |>
+  janitor$clean_names() |>
+  dplyr$filter(
+    stringr$str_detect(tolower(event), "cholera")
+  ) |>
+  dplyr$transmute(
+    iso3 = countrycode$countryname(country, destination = "iso3c"),
+    start_date = lubridate$dmy(
+      dplyr$case_when(
+        !is.na(start_of_reporting_period) ~ start_of_reporting_period,
+        !is.na(start_of_reporting_period_2) ~ start_of_reporting_period_2,
+        !is.na(start_of_reporting_period_3) ~ start_of_reporting_period_3
+      )
+    ),
+    date = as.Date(week_date),
+    cholera_cases = readr$parse_number(total_cases)
+  ) |>
+  dplyr$group_by( # some countries have multiple sets of cases reported each date (DRC)
+    iso3,
+    date
+  ) |>
+  dplyr$summarize(
+    start_date = min(start_date),
+    cholera_cases = sum(cholera_cases),
+    .groups = "drop"
+  ) |>
+  dplyr$group_by(
+    iso3
+  ) |>
+  dplyr$arrange(
+    start_date,
+    date,
+    .by_group = TRUE
+  ) |>
+  dplyr$mutate(
+    cholera_cases = zoo$na.approx(cholera_cases)
+  ) |>
+  dplyr$ungroup()
+
+# flagged data
+
+df_cholera_flags <- df_cholera_wrangled |>
+  dplyr$group_by(iso3) |>
+  dplyr$mutate(
+    flag_1k = lim_alert(cholera_cases, 1000),
+    flag_2k = lim_alert(cholera_cases, 2000),
+    flag_5k = lim_alert(cholera_cases, 5000),
+    flag_10k = lim_alert(cholera_cases, 10000)
+  ) |>
+  dplyr$ungroup() |>
+  tidyr$pivot_longer(
+    flag_1k:flag_10k,
+    names_to = "flag"
+  ) |>
+  dplyr$filter(
+    value
+  ) |>
+  dplyr$group_by(
+    iso3,
+    start_date
+  ) |>
+  dplyr$summarize(
+    end_date = max(date),
+    latest_flag = utils$tail(flag, n = 1),
+    message = paste0(
+      "There have been ",
+      scales$comma_format()(tail(cholera_cases, n = 1)),
+      " cholera cases reported since ",
+      format_date(min(start_date)),
+      "."
+    ),
+    .groups = "drop"
+  ) |>
+  get_country_names() |>
+  dplyr$mutate(
+    flag_type = "cholera",
+    flag_source = "who",
+    .before = start_date
+  )
+
+############################################
+#### COMPARE WITH PREVIOUS FLAGGED DATA ####
+############################################
+
+df_cholera_flags_prev <- gs$read_gs_file(
+  name = "flags_cholera"
+)
+
+df_cholera_flags_new <- dplyr$anti_join(
+  df_cholera_flags,
+  df_cholera_flags_prev,
+  by = c("iso3", "start_date", "end_date")
+) |>
+  dplyr$mutate(
+    email = TRUE
+  )
+
+df_cholera_flags_final <- dplyr$semi_join(
+  df_cholera_flags,
+  df_cholera_flags_prev,
+  by = c("iso3", "start_date", "end_date")
+) |>
+  dplyr$mutate(
+    email = FALSE
+  ) |>
+  dplyr$bind_rows(
+    df_cholera_flags_new
+  ) |>
+  dplyr$mutate(
+    url = NA,
+    .before = email
+  ) |>
+  dplyr$mutate(
+    summary_experimental = NA,
+    .after = email
+  )
+
+########################
+#### SAVE IPC  DATA ####
+########################
+
+gs$update_gs_file(
+  df = df_cholera_raw,
+  name = "raw_cholera"
+)
+
+gs$update_gs_file(
+  df = df_cholera_wrangled,
+  name = "wrangled_cholera"
+)
+
+gs$update_gs_file(
+  df = df_cholera_flags_final,
+  name = "flags_cholera"
+)
---
+++ src/utils/plots/plot_displacement.R
@@ -0,0 +1,106 @@
+box::use(dplyr)
+box::use(lubridate)
+box::use(rlang[`!!`])
+box::use(gghdx)
+box::use(gg = ggplot2)
+box::use(scales)
+
+gghdx$gghdx()
+
+# local modules
+box::use(gs = ../../utils/google_sheets)
+
+#' Plot IDMC displacement data
+#'
+#' Plots IDMC displacement data for a specific country, defined by an ISO3 code.
+#' The wrangled IDMC displacement data is loaded in and filtered to that country.
+#' Displacement is summed to individual weeks and then plotted as a line plot,
+#' with any times that alerts were generated color in light red of the
+#' background of the plot
+#'
+#' @param iso3 ISO3 code.
+#'
+#' @returns Plot of displacement for that country.
+#'
+#' @export
+plot_displacement <- function(
+    iso3
+) {
+
+  # load in the data for plotting
+  df_plot <- gs$read_gs_file(
+    "wrangled_idmc"
+  ) |>
+    dplyr$filter(
+      iso3 == !!iso3
+    )
+
+  # generate flagging times for plotting, to create for each row groups of
+  # consecutive days where a country was actively flagged
+  df_flag_times <- df_plot |>
+    dplyr$mutate(
+      flag_group = cumsum(!alert_any)
+    ) |>
+    dplyr$filter(
+      alert_any
+    ) |>
+    dplyr$group_by(
+      flag_group
+    ) |>
+    dplyr$summarize(
+      flag_start = min(date),
+      flag_end = max(date),
+      .groups = "drop"
+    )
+
+  df_plot |>
+    dplyr$mutate(
+      year = lubridate$year(date),
+      week = lubridate$week(date)
+    ) |>
+    dplyr$group_by(
+      year,
+      week
+    ) |>
+    dplyr$summarize(
+      date = min(date),
+      displacement = sum(displacement_daily),
+      .groups = "drop"
+    ) |>
+    gg$ggplot() +
+    gg$geom_rect(
+      data = df_flag_times,
+      mapping = gg$aes(
+        xmin = flag_start,
+        xmax = flag_end
+      ),
+      ymin = 0,
+      ymax = Inf,
+      fill = gghdx$hdx_hex("tomato-ultra-light")
+    ) +
+    gg$geom_line(
+      gg$aes(
+        x = date,
+        y = displacement
+      )
+    ) +
+    gg$expand_limits(y = 0) +
+    gg$scale_y_continuous(
+      labels = scales$label_comma()
+    ) +
+    gg$scale_x_date(
+      date_breaks = "6 months",
+      labels = scales$label_date_short()
+    ) +
+    gg$theme(
+      axis.text.x = gg$element_text(vjust = 1)
+    ) +
+    gg$labs(
+      x = "",
+      y = "Displacement (weekly)",
+      color = "",
+      title = "Historical weekly displacement",
+      subtitle = "Abnormally high displacement highlighted in light red",
+      caption = "Data from the IDMC, http://www.internal-displacement.org"
+    )
+}
