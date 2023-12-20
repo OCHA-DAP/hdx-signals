c5e3c262a66a5918e019dac0bf366441c1c721bd
Seth Caldwell <caldwellst@gmail.com>
2023-12-20 16:32:26 +0000

    Switch to arrow system of accessing buckets

--- src/utils/cloud_storage.R
+++ src/utils/cloud_storage.R
@@ -1,12 +1,11 @@
 box::use(stringr)
 box::use(readr)
 box::use(arrow)
-box::use(gcs = googleCloudStorageR)
 box::use(httr)
 
 box::use(../utils/gmas_test_run[gmas_test_run])
 
-#' Read a Parquet file stored on Google Drive
+#' Read a Parquet file stored on Google Cloud Platform bucket
 #'
 #' Reads a file from the `global-monitoring` bucket.
 #' The file is read based on its prefix. Currently, the only support is for
@@ -23,54 +22,32 @@ box::use(../utils/gmas_test_run[gmas_test_run])
 #'
 #' @export
 read_gcs_file <- function(name) {
-  gcs$gcs_get_object(
-    object_name = name,
-    bucket = "global-monitoring",
-    parseFunction = gcs_parse_parquet
+  arrow$read_parquet(
+    bucket$path(name)
   )
 }
 
-#' Parse parquet file
-#'
-#' Helper function to parse parquet file.
-#'
-#' @param object Object to parse
-gcs_parse_parquet <- function(object) {
-  con <- gzcon(rawConnection(httr$content(object)))
-  on.exit(close(con))
-  file_raw <- readr$read_file_raw(con)
-  file_buffer <- arrow$buffer(file_raw)
-  arrow$read_parquet(file_buffer)
-}
-
-#' Save data frame on Google Cloud Platform
+#' Write data frame on Google Cloud Platform
 #'
 #' A convenient file saver that saves the data frame to the specified
 #' parquet file. Simply writes out the data frame based on the file extension and
-#' uploads that to the GCS Bucket using [googleCloudStorageR::gcs_upload()].
+#' uploads that to the GCS Bucket using [arrow::gs_bucket()] file systems.
 #' Currently only supports Apache Parquet files.
 #'
 #' Files written out based on file type:
 #'
 #' * Apache Parquet: [arrow::write_parquet()]
 #'
-#' If `gmas_test_run()`, the file is written out to a temporary file but is not
-#' uploaded to the bucket.
+#' If `gmas_test_run()`, the file is not uploaded to the bucket.
 #'
 #' @param df Data frame to save out.
 #' @param name Name of the file to read, including prefix (`input/` or `output/`)
 #'     and filetype `.parquet`.
 #'
-#' @returns Nothing, but file is saved to the `global-monitoring` bucket.
+#' @returns Nothing, but file is written to the `global-monitoring` bucket.
 #'
 #' @export
 update_gcs_file <- function(df, name) {
-  tf <- tempfile(fileext = ".parquet")
-  arrow$write_parquet(
-    x = df,
-    sink = tf
-  )
-
   if (gmas_test_run()) {
     message(
       "`update_pq_file()` not saving data as `gmas_test_run()` is `TRUE`. ",
@@ -80,13 +57,9 @@ update_gcs_file <- function(df, name) {
     return(invisible(NULL))
   }
 
-  invisible(
-    gcs$gcs_upload(
-      file = tf,
-      bucket = "global-monitoring",
-      name = name,
-      predefinedAcl = "bucketLevel"
-    )
+  arrow$write_parquet(
+    x = df,
+    sink = bucket$path(name)
   )
 }
 
@@ -100,17 +73,19 @@ update_gcs_file <- function(df, name) {
 #'
 #' @export
 gcs_file_detect <- function(pattern = NULL) {
-  file_names <- gcs$gcs_list_objects(bucket = "global-monitoring")$name
+  file_names <- bucket$ls(recursive =)
   if (!is.null(pattern)) {
     file_names <- file_names[stringr$str_detect(file_names, pattern)]
   }
   file_names
 }
 
-##############################
-#### SETUP AUTHORIZATIONS ####
-##############################
+######################
+#### SETUP BUCKET ####
+######################
 
-gcs$gcs_auth(
-  json_file = Sys.getenv("GLOBAL_MONITORING_JSON")
+# this bucket used to read and write from the GCS parquet
+bucket <- arrow$gs_bucket(
+  bucket = "global-monitoring",
+  json_credentials = Sys.getenv("GLOBAL_MONITORING_JSON")
 )
