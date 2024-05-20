c37b188fdcbc91c85f484b4973fd04c7d11a560b
hannahker <hannah.ker96@gmail.com>
2024-05-20 13:00:13 -0700

    Temporarily bypass ACLED data update

--- src-static/update_acled_info.R
+++ src-static/update_acled_info.R
@@ -9,48 +9,54 @@ box::use(logger[log_info])
 box::use(../src/utils/country_codes)
 box::use(cs = ../src/utils/cloud_storage)
 
-log_info("Updating ACLED info...")
+# TODO: Remove once this passes from GH runner
+if (interactive()) {
+  log_info("Updating ACLED info...")
 
-# read excel file of acled country codes from their website
-download.file(
-  url = "https://acleddata.com/download/3987/",
-  destfile = tf <- tempfile(fileext = ".xlsx"),
-  quiet = TRUE
-)
-
-df <- readxl$read_excel(tf)
-
-# turn start dates into dates and get iso3 codes
-df_acled_info <- df |>
-  dplyr$mutate(
-    Country = ifelse(
-      Country == "Saint Vincent and Grenadines",
-      "Saint Vincent and the Grenadines", # to match HDX
-      Country
-    )
-  ) |>
-  dplyr$transmute(
-    iso3 = country_codes$ison_to_iso3(`ISO Codes`),
-    start_date = as.Date(
-      x = paste(1, `Start Date`),
-      format = "%d %B %Y"
-    ),
-    acled_hdx_country = Country |>
-      stringr$str_remove_all("[^[:alnum:]\\s-\\.]") |>
-      stringr$str_remove_all("\\.$") |>
-      tolower() |>
-      stringr$str_replace_all("\\s|\\.", "-"),
-    acled_hdx_url = as.character(
-      glue$glue(
-        "https://data.humdata.org/dataset/{acled_hdx_country}-acled-conflict-data"
-      )
-    )
+  # read excel file of acled country codes from their website
+  download.file(
+    url = "https://acleddata.com/download/3987/",
+    destfile = tf <- tempfile(fileext = ".xlsx"),
+    quiet = TRUE
   )
 
-fname <- "input/acled_info.parquet"
-cs$update_az_file(
-  df = df_acled_info,
-  name = fname
-)
+  df <- readxl$read_excel(tf)
 
-log_info(paste0("Successfully downloaded ACLED info and saved to ", fname))
+  # turn start dates into dates and get iso3 codes
+  df_acled_info <- df |>
+    dplyr$mutate(
+      Country = ifelse(
+        Country == "Saint Vincent and Grenadines",
+        "Saint Vincent and the Grenadines", # to match HDX
+        Country
+      )
+    ) |>
+    dplyr$transmute(
+      iso3 = country_codes$ison_to_iso3(`ISO Codes`),
+      start_date = as.Date(
+        x = paste(1, `Start Date`),
+        format = "%d %B %Y"
+      ),
+      acled_hdx_country = Country |>
+        stringr$str_remove_all("[^[:alnum:]\\s-\\.]") |>
+        stringr$str_remove_all("\\.$") |>
+        tolower() |>
+        stringr$str_replace_all("\\s|\\.", "-"),
+      acled_hdx_url = as.character(
+        glue$glue(
+          "https://data.humdata.org/dataset/{acled_hdx_country}-acled-conflict-data"
+        )
+      )
+    )
+
+  fname <- "input/acled_info.parquet"
+  cs$update_az_file(
+    df = df_acled_info,
+    name = fname
+  )
+
+  log_info(paste0("Successfully downloaded ACLED info and saved to ", fname))
+
+} else {
+  log_info("Skipping ACLED data download")
+}
