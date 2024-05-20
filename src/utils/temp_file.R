5fb7fb2f510e0c7c4de3dcbb7e8b7437d84b6319
Seth Caldwell <caldwellst@gmail.com>
2024-05-20 21:59:58 +0100

    Implement capacity to view images in local tests (#96)

---
+++ src/utils/temp_file.R
@@ -0,0 +1,31 @@
+box::use(here)
+
+#' Temporary file for use across modules
+#'
+#' Used to allow us to view preview campaign HTML files with images, because they
+#' can only be viewed if they are stored in the same directory as the HTML file.
+#' Simply creates tempfiles in the `.tempdir` folder.
+#'
+#' This folder is used rather than `tempdir()` to ensure it exists across
+#' module runs. Issues were encountered with `tempdir()` disappearing between
+#' module calls so we couldn't place the HTML and images in the same folder.
+#'
+#' @param fileext File extension, passed to `temp.file()`.
+#'
+#' @examples
+#' temp_file(".csv")
+#'
+#' @return Filepath
+#'
+#' @export
+temp_file <- function(fileext = "") {
+  tempfile(
+    tmpdir = temp_dir,
+    fileext = fileext
+  )
+}
+
+# ensure directory is created and empty when this module is loaded
+temp_dir <- here$here(".temp_dir")
+dir.create(temp_dir, showWarnings = FALSE)
+unlink(paste0(temp_dir, "/*"), recursive = TRUE)
