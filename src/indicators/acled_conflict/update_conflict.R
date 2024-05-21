c61375060a5049d8cc035f4961e02003843579c1
hannahker <hannah.ker96@gmail.com>
2024-05-21 11:31:36 -0700

    Add logging to conflict and food insecurity indicators

--- src/indicators/acled_conflict/update_conflict.R
+++ src/indicators/acled_conflict/update_conflict.R
@@ -1,4 +1,4 @@
-# indiciator utilities
+# indicator utilities
 box::use(./utils/raw_conflict)
 box::use(./utils/wrangle_conflict)
 box::use(./utils/alert_conflict)
@@ -10,9 +10,18 @@ box::use(./utils/summary_conflict)
 # generate signals workhorse
 box::use(../../alerts/generate_signals[generate_signals])
 
+box::use(logger[log_info])
+
 # get data
 df_raw <- raw_conflict$raw()
 df_wrangled <- wrangle_conflict$wrangle(df_raw)
+
+test <- as.logical(Sys.getenv("TEST", unset = FALSE))
+
+log_info("Checking acled conflict indicator... Running with:")
+log_info(paste0("GMAS_TEST_RUN = ", Sys.getenv("GMAS_TEST_RUN")))
+log_info(paste0("TEST = ", Sys.getenv("TEST")))
+log_info(paste0("FIRST_RUN = ", Sys.getenv("FIRST_RUN")))
 
 # now generate signals
 generate_signals(
@@ -23,5 +32,8 @@ generate_signals(
   plot_fn = plot_conflict$plot,
   info_fn = info_conflict$info,
   map_fn = map_conflict$map,
-  summary_fn = summary_conflict$summary
+  summary_fn = summary_conflict$summary,
+  test = test
 )
+
+log_info("Successfully checked acled conflict indicator")
--- src/indicators/ipc_food_insecurity/update_food_insecurity.R
+++ src/indicators/ipc_food_insecurity/update_food_insecurity.R
@@ -9,8 +9,17 @@ box::use(./utils/info_food_insecurity)
 box::use(../../alerts/generate_signals[generate_signals])
 box::use(../../alerts/triage_signals[triage_signals])
 
+box::use(logger[log_info])
+
 df_raw <- raw_food_insecurity$raw()
 df_wrangled <- wrangle_food_insecurity$wrangle(df_raw)
+
+test <- as.logical(Sys.getenv("TEST", unset = FALSE))
+
+log_info("Checking food insecurity indicator... Running with:")
+log_info(paste0("GMAS_TEST_RUN = ", Sys.getenv("GMAS_TEST_RUN")))
+log_info(paste0("TEST = ", Sys.getenv("TEST")))
+log_info(paste0("FIRST_RUN = ", Sys.getenv("FIRST_RUN")))
 
 # now generate signals
 df_ipc <- generate_signals(
@@ -20,5 +29,8 @@ df_ipc <- generate_signals(
   plot_fn = plot_food_insecurity$plot,
   summary_fn = summary_food_insecurity$summary,
   map_fn = map_food_insecurity$map,
-  info_fn = info_food_insecurity$info
+  info_fn = info_food_insecurity$info,
+  test = test
 )
+
+log_info("Successfully checked food insecurity indicator")
