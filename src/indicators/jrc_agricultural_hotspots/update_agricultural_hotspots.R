355f0f43a65d53c39a5b0ba51aa3d9db11e322b7
hannahker <hannah.ker96@gmail.com>
2024-05-21 11:34:48 -0700

    Add logging to jrd indicator

--- src/indicators/jrc_agricultural_hotspots/update_agricultural_hotspots.R
+++ src/indicators/jrc_agricultural_hotspots/update_agricultural_hotspots.R
@@ -8,8 +8,17 @@ box::use(./utils/plot_agricultural_hotspots)
 box::use(../../alerts/generate_signals[generate_signals])
 box::use(../../alerts/triage_signals[triage_signals])
 
+box::use(logger[log_info])
+
 df_raw <- raw_agricultural_hotspots$raw()
 df_wrangled <- wrangle_agricultural_hotspots$wrangle(df_raw)
+
+test <- as.logical(Sys.getenv("TEST", unset = FALSE))
+
+log_info("Checking agricultural hotspots indicator... Running with:")
+log_info(paste0("GMAS_TEST_RUN = ", Sys.getenv("GMAS_TEST_RUN")))
+log_info(paste0("TEST = ", Sys.getenv("TEST")))
+log_info(paste0("FIRST_RUN = ", Sys.getenv("FIRST_RUN")))
 
 # now generate signals
 generate_signals(
@@ -18,5 +27,8 @@ generate_signals(
   alert_fn = alert_agricultural_hotspots$alert,
   summary_fn = summary_agricultural_hotspots$summary,
   info_fn = info_agricultural_hotspots$info,
-  plot_fn = plot_agricultural_hotspots$plot
+  plot_fn = plot_agricultural_hotspots$plot,
+  test = test
 )
+
+log_info("Successfully checked agricultural hotspots indicator")
