24ca3ab8c89635d2cf233ad2d829647abaa4bde4
Zack <zackarno@gmail.com>
2024-05-22 14:38:39 -0400

    func to create buffer by approx % metric

--- src/utils/assertions.R
+++ src/utils/assertions.R
@@ -1,4 +1,79 @@
+box::use(dplyr)
+box::use(purrr)
+
 box::use(sf)
+box::use(./get_iso3_sf) # just using for dev-testing examples - can probably remove later when I switch to sf internal data for examples
+
+
+#' assert_in_buffered_geom
+#'
+#' @param geom sf geometry. In this use case it will likely always be the admin 0 boundary
+#'     returned from get_iso3_sf$get_iso3_sf(iso3, "adm0")
+#' @param geom_buffer_pct choose an approximate % to buffere each polygon element of the geom by
+#' @param x 2nd geom to test whether it fits entirely in the geom after being buffered
+#'
+#' @return
+#' @export
+#'
+#' @examples
+assert_in_buffered_geom <- function(
+    geom,
+    geom_buffer_pct,
+    x
+){
+  geom_buffered <- buffer_by_pct(
+    geom= geom,
+    pct =  geom_buffer_pct
+  )
+  # pseudo code begins
+  # sf$st_within(x, geom_buffered)
+  # if not all x within geom_buffered
+  # create alert
+}
+
+
+#' buffer_by_pct
+#' given a POLYGON or MULTIPOLYGON buffer each each polygon by a % of the length
+#' of it's bbox hypotenous
+#' @param x POLYGON or MULTIPOLYGON
+#' @param pct `numeric` percentage to buffer by
+#'
+#' @return POLYGON or MULTIPOLYGON
+#' @examples
+#' bgd_adm0 <- get_iso3_sf$get_iso3_sf("BGD", "adm0")
+#' buffer_by_pct(bgd_adm0,0.1)
+
+buffer_by_pct <-  function(geom,pct=0.1){
+
+  conversion_factor <- 111132  # meters per degree (approximately)
+
+  geom_cast <- sf$st_cast(geom,to = "POLYGON")
+  geom_diag_length <- sqrt(sf$st_area(geom_cast))/conversion_factor
+
+  geom_cast <- geom_cast |>
+    dplyr$mutate(
+      diag_approx = as.numeric(geom_diag_length),
+      buffer_dist = diag_approx * pct
+    )
+
+  geom_cast_buffered <- geom_cast |>
+    dplyr$transmute(
+      buffer_geom = sf$st_buffer(x = geom_cast$geometry, dist = buffer_dist)
+    )
+
+  # obj has two geometries so we should set the recognized geometry
+  # to the buffered shapes and drop the other one
+  # then we dissolve
+  geom_cast_buffered |>
+    sf$st_set_geometry("buffer_geom") |>
+    dplyr$select(-geometry) |>
+    dplyr$rename(
+      geometry = "buffer_geom"
+    ) |>
+    dplyr$summarise()
+}
+
+
 
 #' Assert the bbox of 2 geometries overlap
 #'
@@ -39,8 +114,6 @@ box::use(sf)
 #'
 #' assert_bbox_overlap(pts_1, pts_2)
 #' assert_bbox_overlap(pts_1, pts_3)
-#' st_intersects(pts_1,pts_2)
-#' st_intersects(pts_1,pts_3)
 assert_bbox_overlap <- function(x, y) {
   x_bbox <- sf$st_bbox(x)
   y_bbox <- sf$st_bbox(y)
@@ -58,6 +131,62 @@ assert_bbox_overlap <- function(x, y) {
          paste(names(y_bbox), y_bbox, sep = "=", collapse = ", "),
          ".")
   } else {
+    return(TRUE)
+  }
+}
+
+
+#' @examples
+#'  poly_1 <- sf$st_polygon(
+#'    list(
+#'      cbind(
+#'        c(12.185373, 12.255774, 12.728886, 12.947403, 12.672345, 12.635662, 12.185373),
+#'        c(12.529575, 11.939943, 11.967415, 12.21874, 12.435015, 12.558468, 12.529575)
+#'      )
+#'    )
+#'  )
+#'  poly_2 <- st_polygon(
+#'    list(
+#'      cbind(
+#'        c(12.240334, 12.26224, 12.913666, 12.240334),
+#'        c(12.510266, 12.021816, 12.527339, 12.510266)
+#'      )
+#'    )
+#'  ) |>
+#'    st_sfc(crs=4326)
+#'
+#'  assert_geom_containment(
+#'    x= poly_1,
+#'    y=pts_1
+#'    )
+#'
+#'  assert_geom_containment(
+#'    x= poly_2,
+#'    y=pts_1
+#'    )
+#'
+#'  assert_geom_containment(x = pts_1,
+#'                          y = pts_2
+#'                          )
+
+
+assert_geom_containment <- function(x,y) {
+  x_bbox <- sf$st_bbox(x)
+  y_bbox <- sf$st_bbox(y)
+
+  x_bbox_contains_ybbox <-
+    y_bbox$xmin >= x_bbox$xmin &&
+    y_bbox$xmax <= x_bbox$xmax &&
+    y_bbox$ymin >= x_bbox$ymin &&
+    y_bbox$ymax <= x_bbox$ymax
+
+  if (!(
+    x_bbox_contains_ybbox
+  )) {
+    stop("The bounding box of the provided sf object provided as y is not contained within the bounding box of sf object provided as x.",
+         call. = FALSE)
+  }
+  else {
     return(TRUE)
   }
 
