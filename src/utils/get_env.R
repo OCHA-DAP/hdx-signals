5b7cea42fc93fd02376eb34e6cecf66d017da562
hannahker <hannah.ker96@gmail.com>
2024-05-23 16:12:46 -0700

    Simplify logic

--- src/utils/get_env.R
+++ src/utils/get_env.R
@@ -11,7 +11,7 @@ logger$configure_logger()
 #' @export
 get_env <- function(env_var_name, output = TRUE) {
   val <- Sys.getenv(env_var_name)
-  if (nzchar(val) == FALSE) {
+  if (!nzchar(val)) {
     error_message <- sprintf("Environment variable '%s' is empty or not set.", env_var_name)
     log_error(error_message)
     stop()
