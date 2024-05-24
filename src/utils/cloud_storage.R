00ba6c92f318f9ff9fb53bf2f025ac1bdb4a7d4a
hannahker <hannah.ker96@gmail.com>
2024-05-23 17:27:08 -0700

    Add back logging import

--- src/utils/cloud_storage.R
+++ src/utils/cloud_storage.R
@@ -9,6 +9,7 @@ box::use(tools)
 box::use(readr)
 box::use(jsonlite)
 box::use(dplyr)
+box::use(logger[log_debug])
 
 box::use(../utils/gmas_test_run[gmas_test_run])
 box::use(../utils/get_env[get_env])
