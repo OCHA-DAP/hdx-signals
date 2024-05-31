612b4a22237304768a6d340a6b6fa66ab9ec2843
hannahker <hannah.ker96@gmail.com>
2024-05-31 14:01:40 -0700

    Rename agricultural hotspots test

--- test/automated/test_update_jrc_agricultural_hotspots.R
+++ test/automated/test_update_jrc_agricultural_hotspots.R
@@ -1,14 +1,13 @@
-box::use(assertthat)
+box::use(testthat)
+box::use(httptest)
 
+box::use(../../src/email/mailchimp/base_api)
+box::use(../../src/alerts/generate_signals[generate_signals])
 box::use(../../src/indicators/jrc_agricultural_hotspots/utils/wrangle_agricultural_hotspots)
 box::use(../../src/indicators/jrc_agricultural_hotspots/utils/alert_agricultural_hotspots)
 box::use(../../src/indicators/jrc_agricultural_hotspots/utils/summary_agricultural_hotspots)
 box::use(../../src/indicators/jrc_agricultural_hotspots/utils/info_agricultural_hotspots)
 box::use(../../src/indicators/jrc_agricultural_hotspots/utils/plot_agricultural_hotspots)
-
-box::use(../../src/alerts/generate_signals[generate_signals])
-box::use(../../src/utils/hs_logger)
-hs_logger$configure_logger()
 
 # Create a simple wrapper function so that we can just pass in the wrangled df
 generate_signals_wrapper <- function(df_wrangled) {
@@ -24,12 +23,10 @@ generate_signals_wrapper <- function(df_wrangled) {
   )
 }
 
-# Test basic case
-df_jrc <- readRDS("test/data/jrc_hotspots_basic.RDS") |>
-  wrangle_agricultural_hotspots$wrangle() |>
-  generate_signals_wrapper()
-
-assertthat$assert_that(nrow(df_jrc) == 0, msg = "Output DataFrame should not have any signals")
-assertthat$assert_that(ncol(df_jrc) == 38, msg = "Output DataFrame does not have the correct number of columns")
-
-# TODO: Create additional test cases with different input dfs
+testthat$test_that("the base case for JRC hotspots runs and doesn't create any signals", {
+  df_jrc <- readRDS("test/data/jrc_hotspots_basic.RDS") |>
+    wrangle_agricultural_hotspots$wrangle() |>
+    generate_signals_wrapper()
+  testthat$expect_equal(nrow(df_jrc), 0)
+  testthat$expect_equal(ncol(df_jrc), 38)
+})
