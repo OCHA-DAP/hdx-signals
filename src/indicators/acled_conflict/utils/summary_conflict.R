4c289908cf07522a30e29b0aa2014b9d23895e2b
Seth Caldwell <caldwellst@gmail.com>
2024-06-06 14:29:47 +0100

    Use raw text prompts

--- src/indicators/acled_conflict/utils/summary_conflict.R
+++ src/indicators/acled_conflict/utils/summary_conflict.R
@@ -4,7 +4,28 @@ box::use(purrr)
 
 box::use(../../../../src/utils/ai_summarizer)
 
-#' Add campaign info to cholera alerts
+# prompts to be used below
+prompt_short <- readLines(
+  file.path(
+    "src",
+    "indicators",
+    "acled_conflict",
+    "prompts",
+    "short.txt"
+  )
+) |> paste(collapse = " ")
+
+prompt_long <- readLines(
+  file.path(
+    "src",
+    "indicators",
+    "acled_conflict",
+    "prompts",
+    "long.txt"
+  )
+) |> paste(collapse = " ")
+
+#' Add campaign info to ACLED conflict data
 #'
 #' @returns Data frame with campaign information
 #'
@@ -31,22 +52,6 @@ summary <- function(df_alerts, df_wrangled, df_raw) {
     ) |>
     dplyr$summarize(
       event_info = paste(notes, collapse = " "),
-      prompt_long = paste(
-        "In a short paragraph, please summarize the main reasons for displacement.",
-        "Avoid providing specific numbers or dates, just provide the general",
-        "reasons behind the displacement and other key qualitative information.",
-        "Only use the information below:\n\n"
-      ),
-      prompt_short = paste(
-        "Please condense the following information into a single 10 word line,",
-        "similar to text you might see on a news ticker. Outputs could look like",
-        "the following 2 examples:",
-        "'Conflict between agropasturalists surges in the northwest' or",
-        "'Military offensive in the south sees surge in conflict fatalities'.",
-        "Expect the reader to have no context, but this is",
-        "intended to capture their attention, so keep the messaging simple, clear",
-        "and punchy. Use only the information below in your summary:\n\n"
-      ),
       .groups = "drop"
     ) |>
     dplyr$mutate(
