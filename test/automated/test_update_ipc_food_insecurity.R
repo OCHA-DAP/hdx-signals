0858949ec4254b2355dea3edc4277aa3dbec0748
hannahker <hannah.ker96@gmail.com>
2024-06-10 14:29:14 +0200

    Add mock dir for ipc tests

--- test/automated/test_update_ipc_food_insecurity.R
+++ test/automated/test_update_ipc_food_insecurity.R
@@ -28,10 +28,12 @@ generate_signals_wrapper <- function(df_wrangled) {
 # to mock API requests to IPC API
 options(httptest.verbose = TRUE)
 
-testthat$test_that("the base case for IPC food insecurity runs and doesn't create any signals", {
-  df_ipc <- readRDS("test/data/ipc_basic.RDS") |>
-    wrangle_food_insecurity$wrangle() |>
-    generate_signals_wrapper()
-  testthat$expect_equal(nrow(df_ipc), 0)
-  testthat$expect_equal(ncol(df_ipc), 38)
+httptest$with_mock_dir("test/mock_apis", {
+  testthat$test_that("the base case for IPC food insecurity runs and doesn't create any signals", {
+    df_ipc <- readRDS("test/data/ipc_basic.RDS") |>
+      wrangle_food_insecurity$wrangle() |>
+      generate_signals_wrapper()
+    testthat$expect_equal(nrow(df_ipc), 0)
+    testthat$expect_equal(ncol(df_ipc), 38)
+  })
 })
