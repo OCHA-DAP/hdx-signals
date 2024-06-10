e867fd69104c8fcd97fd9c976a9d958cb65bdff6
Seth Caldwell <caldwellst@gmail.com>
2024-06-10 23:36:59 +0100

    Improve FS maps

--- src/indicators/ipc_food_insecurity/utils/map_food_insecurity.R
+++ src/indicators/ipc_food_insecurity/utils/map_food_insecurity.R
@@ -99,6 +99,9 @@ food_insecurity_map <- function(df_wrangled, df_raw, title, date) {
             admin_type == "hhg" ~ "Household group",
             .default = "Rural"
           )
+        ) |>
+        dplyr$filter( # drop some extremely complex shapes
+          !(stringr$str_detect(sf$st_geometry_type(geometry), "POINT") & is.na(admin_type))
         )
     },
     error = \(e) NULL
