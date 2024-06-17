8ba3ddecc4107efe4dbf4566a335550f2864ef9c
hannahker <hannah.ker96@gmail.com>
2024-06-17 15:19:35 -0400

    Add basic tests

---
+++ test/automated/test_update_ipc_food_insecurity.R
@@ -0,0 +1,39 @@
+box::use(testthat)
+box::use(httptest)
+
+box::use(../../src/indicators/ipc_food_insecurity/utils/wrangle_food_insecurity)
+box::use(../../src/indicators/ipc_food_insecurity/utils/alert_food_insecurity)
+box::use(../../src/indicators/ipc_food_insecurity/utils/plot_food_insecurity)
+box::use(../../src/indicators/ipc_food_insecurity/utils/summary_food_insecurity)
+box::use(../../src/indicators/ipc_food_insecurity/utils/map_food_insecurity)
+box::use(../../src/indicators/ipc_food_insecurity/utils/info_food_insecurity)
+box::use(../../src/signals/generate_signals[generate_signals])
+
+# Create a simple wrapper function so that we can just pass in the wrangled df
+generate_signals_wrapper <- function(df_wrangled) {
+  generate_signals(
+    df_wrangled = df_wrangled,
+    indicator_id = "ipc_food_insecurity",
+    alert_fn = alert_food_insecurity$alert,
+    plot_fn = plot_food_insecurity$plot,
+    summary_fn = summary_food_insecurity$summary,
+    map_fn = map_food_insecurity$map,
+    info_fn = info_food_insecurity$info,
+    dry_run = FALSE,
+    dry_run_filter = NULL
+  )
+}
+
+# TODO: Work on wrapping with httptest$with_mock_dir("test/mock_apis", {)
+# to mock API requests to IPC API
+options(httptest.verbose = TRUE)
+
+httptest$with_mock_dir("test/mock_apis", {
+  testthat$test_that("the base case for IPC food insecurity runs and doesn't create any signals", {
+    df_ipc <- readRDS("test/data/ipc_basic.RDS") |>
+      wrangle_food_insecurity$wrangle() |>
+      generate_signals_wrapper()
+    testthat$expect_equal(nrow(df_ipc), 0)
+    testthat$expect_equal(ncol(df_ipc), 38)
+  })
+})
---
+++ test/automated/test_update_jrc_agricultural_hotspots.R
@@ -0,0 +1,32 @@
+box::use(testthat)
+box::use(httptest)
+
+box::use(../../src/email/mailchimp/base_api)
+box::use(../../src/signals/generate_signals[generate_signals])
+box::use(../../src/indicators/jrc_agricultural_hotspots/utils/wrangle_agricultural_hotspots)
+box::use(../../src/indicators/jrc_agricultural_hotspots/utils/alert_agricultural_hotspots)
+box::use(../../src/indicators/jrc_agricultural_hotspots/utils/summary_agricultural_hotspots)
+box::use(../../src/indicators/jrc_agricultural_hotspots/utils/info_agricultural_hotspots)
+box::use(../../src/indicators/jrc_agricultural_hotspots/utils/plot_agricultural_hotspots)
+
+# Create a simple wrapper function so that we can just pass in the wrangled df
+generate_signals_wrapper <- function(df_wrangled) {
+  generate_signals(
+    df_wrangled = df_wrangled,
+    indicator_id = "jrc_agricultural_hotspots",
+    alert_fn = alert_agricultural_hotspots$alert,
+    summary_fn = summary_agricultural_hotspots$summary,
+    info_fn = info_agricultural_hotspots$info,
+    plot_fn = plot_agricultural_hotspots$plot,
+    dry_run = FALSE,
+    dry_run_filter = NULL
+  )
+}
+
+testthat$test_that("the base case for JRC hotspots runs and doesn't create any signals", {
+  df_jrc <- readRDS("test/data/jrc_hotspots_basic.RDS") |>
+    wrangle_agricultural_hotspots$wrangle() |>
+    generate_signals_wrapper()
+  testthat$expect_equal(nrow(df_jrc), 0)
+  testthat$expect_equal(ncol(df_jrc), 38)
+})
