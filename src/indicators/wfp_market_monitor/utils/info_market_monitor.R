fcd89a45afd0845cce66cc306eb1a93f20225619
Seth Caldwell <caldwellst@gmail.com>
2024-07-02 16:11:35 +0100

    Ensure soruce_url is character vector

--- src/indicators/wfp_market_monitor/utils/info_market_monitor.R
+++ src/indicators/wfp_market_monitor/utils/info_market_monitor.R
@@ -10,12 +10,12 @@ info <- function(df_alerts, df_wrangled, df_raw) {
   df_alerts |>
     dplyr$transmute(
       hdx_url = "https://data.humdata.org/dataset/global-market-monitor",
-      source_url = glue$glue("https://dataviz.vam.wfp.org/CountryPage_overview?iso3={tolower(iso3)}"),
+      source_url = paste0("https://dataviz.vam.wfp.org/CountryPage_overview?iso3=", tolower(iso3)),
       other_urls = "https://dataviz.vam.wfp.org/reports?current_page=1&theme=8",
       further_information = as.character(
         glue$glue(
           'Access the data directly <a href="{hdx_url}">on HDX</a> ',
-          'and visit the <a href="{source_url}> {location} overview</a> to find ',
+          'and visit the <a href="{source_url}"> {location} overview</a> to find ',
           "key price data and other insights and trends. The most recent reports ",
           "and analyses are available on the Global Market Monitor ",
           '<a href="{other_urls}">Reports Explorer</a>.'
