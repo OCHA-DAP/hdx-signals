f3b07f89daa70bdd3159084e514797eb7ae4d609
Seth Caldwell <caldwellst@gmail.com>
2024-07-02 16:04:31 +0100

    Update market monitor info

--- src/indicators/wfp_market_monitor/utils/info_market_monitor.R
+++ src/indicators/wfp_market_monitor/utils/info_market_monitor.R
@@ -10,14 +10,15 @@ info <- function(df_alerts, df_wrangled, df_raw) {
   df_alerts |>
     dplyr$transmute(
       hdx_url = "https://data.humdata.org/dataset/global-market-monitor",
-      source_url = "https://dataviz.vam.wfp.org/reports?current_page=1&theme=8",
-      other_urls = NA_character_,
+      source_url = glue$glue("https://dataviz.vam.wfp.org/CountryPage_overview?iso3={tolower(iso3)}"),
+      other_urls = "https://dataviz.vam.wfp.org/reports?current_page=1&theme=8",
       further_information = as.character(
         glue$glue(
           'Access the data directly <a href="{hdx_url}">on HDX</a> ',
-          "and visit the Global Market Monitor ",
-          '<a href="{source_url}">Reports Explorer</a> to access the ',
-          "most recent information and analysis."
+          'and visit the <a href="{source_url}> {location} overview</a> to find ',
+          "key price data and other insights and trends. The most recent reports ",
+          "and analyses are available on the Global Market Monitor ",
+          '<a href="{other_urls}">Reports Explorer</a>.'
         )
       )
     )
