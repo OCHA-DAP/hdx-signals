736e8c71c9bd31d441308b11ee1d64f533f15870
Seth Caldwell <caldwellst@gmail.com>
2024-07-02 10:43:35 +0100

    Check linting

--- src/utils/get_env.R
+++ src/utils/get_env.R
@@ -9,6 +9,7 @@ hs_logger$configure_logger()
 #' @param env_var_name Name of environment variable
 #'
 #' @returns Value of environment variable, unless it is not set
+#'
 #' @export
 get_env <- function(env_var_name, output = TRUE) {
   val <- Sys.getenv(env_var_name)
