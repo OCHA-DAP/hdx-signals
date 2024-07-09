81a0d96c093f61201bc73a0fcc1a531c44c2af3f
Seth Caldwell <caldwellst@gmail.com>
2024-07-09 11:11:14 +0100

    Drop the closing text

--- src-static/update_indicator_mapping.R
+++ src-static/update_indicator_mapping.R
@@ -22,15 +22,7 @@ df <- dplyr$bind_rows(
     indicator_subject = "Conflict-driven displacement",
     static_segment = 25093,
     banner_url = "https://mcusercontent.com/ea3f905d50ea939780139789d/images/9f24c42f-e363-9843-0789-c5a2ef4c1e94.png", #nolint
-    data_source = "Displacement data - IDMC",
-    closing_text = paste(
-      'The <a href="https://www.internal-displacement.org">Internal Displacement',
-      "Monitoring Centre (IDMC)</a> provides high-quality data, analysis and expertise",
-      "on internal displacement to inform policy and operational decisions that can",
-      "improve the lives of internally displaced people worldwide and reduce",
-      "the risk of future displacement. This signal uses IDMC events data reporting",
-      "displacements driven by conflict."
-    )
+    data_source = "Displacement data - IDMC"
   ),
   dplyr$tibble( # IDMC displacement disaster
     indicator_id = "idmc_displacement_disaster",
@@ -40,15 +32,7 @@ df <- dplyr$bind_rows(
     indicator_subject = "Disaster-driven displacement",
     static_segment = 25097,
     banner_url = "https://mcusercontent.com/ea3f905d50ea939780139789d/images/9f24c42f-e363-9843-0789-c5a2ef4c1e94.png", #nolint
-    data_source = "Displacement data - IDMC",
-    closing_text = paste(
-      'The <a href="https://www.internal-displacement.org">Internal Displacement',
-      "Monitoring Centre (IDMC)</a> provides high-quality data, analysis and expertise",
-      "on internal displacement to inform policy and operational decisions that can",
-      "improve the lives of internally displaced people worldwide and reduce",
-      "the risk of future displacement. This signal uses IDMC events data reporting",
-      "displacements driven by natural disaster or other non-conflict hazards."
-    )
+    data_source = "Displacement data - IDMC"
   ),
   dplyr$tibble( # IPC food insecurity
     indicator_id = "ipc_food_insecurity",
@@ -58,15 +42,7 @@ df <- dplyr$bind_rows(
     indicator_subject = "Food insecurity",
     static_segment = 25101,
     banner_url = "https://mcusercontent.com/ea3f905d50ea939780139789d/images/acc34c0e-ad04-3e80-a1a0-1c9a4a664926.png", #nolint
-    data_source = "Food insecurity - IPC/CH",
-    closing_text = paste(
-      '<a href="http://cadreharmonise.org">Cadre Harmonisé (CH)</a> and',
-      '<a href="https://www.ipcinfo.org">Integrated Food Security Phase Classification (IPC)</a>',
-      "are analytical frameworks which synthesize indicators of food and nutrition",
-      "security outcomes and the inference of contributing factors into scales and",
-      "figures representing the nature and severity of crisis and implications for",
-      "strategic response in food security and nutrition."
-    )
+    data_source = "Food insecurity - IPC/CH"
   ),
   dplyr$tibble( # WHO cholera
     indicator_id = "who_cholera",
@@ -76,11 +52,7 @@ df <- dplyr$bind_rows(
     indicator_subject = "Cholera",
     static_segment = 25105,
     banner_url = NA_character_,
-    data_source = "Cholera data - WHO",
-    closing_text = paste(
-      "The World Health Organization (WHO) weekly bulletins for the WHO African",
-      "Region cover outbreaks and other emergencies across the region."
-    )
+    data_source = "Cholera data - WHO"
   ),
   dplyr$tibble( # JRC agricultural hotspots
     indicator_id = "jrc_agricultural_hotspots",
@@ -90,14 +62,7 @@ df <- dplyr$bind_rows(
     indicator_subject = "Agricultural hotspots",
     static_segment = 25109,
     banner_url = "https://mcusercontent.com/ea3f905d50ea939780139789d/images/c13c0ad9-5a19-6bf1-b759-9cb83b37905e.png", #nolint
-    data_source = "Agricultural hotspots - JRC ASAP",
-    closing_text = paste(
-      '<a href="https://agricultural-production-hotspots.ec.europa.eu/index.php">',
-      "Anomaly Hotspots of Agricultural Production (ASAP)</a> is an online decision support",
-      "system for early warning about hotspots of agricultural production anomaly",
-      "(crop and rangeland), developed by the Joint Research Centre (JRC) for food security",
-      "crises prevention and response planning."
-    )
+    data_source = "Agricultural hotspots - JRC ASAP"
   ),
   dplyr$tibble( # WFP market monitoring
     indicator_id = "wfp_market_monitor",
@@ -107,19 +72,7 @@ df <- dplyr$bind_rows(
     indicator_subject = "Global Market Monitor",
     static_segment = 25113,
     banner_url = "https://mcusercontent.com/ea3f905d50ea939780139789d/images/91c6f44c-dcce-3982-406a-0316c1bd30ec.png", #nolint
-    data_source = "Global Market Monitor - WFP",
-    closing_text = paste(
-      "Changes in the cost of food baskets in WFP's ",
-      '<a href="https://www.wfp.org/publications/market-monitor">Global Market Monitor</a>',
-      "are based on a standardized methodology for comparability across countries. These ",
-      "figures can therefore differ from what WFP country offices report in their market ",
-      "bulletions or from food basket cost changes shown on WFP's ",
-      '<a href="https://dataviz.vam.wfp.org/economic/inflation?current_page=1">Economic ',
-      "Explorer</a>, which are based on baskets defined individually by country offices. ",
-      "For more details, please refer to the ",
-      '<a href="https://dataviz.vam.wfp.org/reports/global-coverage-global-market-monitor-mar-2024?_ga=2.216546710.1452952701.1719219215-2028703503.1710344334">', # nolint
-      "Methodology & Data page</a>."
-    )
+    data_source = "Global Market Monitor - WFP"
   ),
   dplyr$tibble( # ACLED conflict
     indicator_id = "acled_conflict",
@@ -129,16 +82,7 @@ df <- dplyr$bind_rows(
     indicator_subject = "Armed conflict",
     static_segment = 25117,
     banner_url = "https://mcusercontent.com/ea3f905d50ea939780139789d/images/86ba2e73-ddc6-5555-b903-6de9cfb82ec1.png", #nolint
-    data_source = "Armed conflict - ACLED",
-    closing_text = paste(
-      '<a href="https://acleddata.com/">The Armed Conflict Location & Event Data',
-      "Project (ACLED)</a> is a disaggregated",
-      "data collection, analysis, and crisis mapping project. ACLED collects the",
-      "dates, actors, locations, fatalities, and types of all reported political",
-      "violence and protest events in more than",
-      '<a href="https://acleddata.com/download/4404/">200 countries and territories',
-      "</a> in real time."
-    )
+    data_source = "Armed conflict - ACLED"
   )
 )
 
