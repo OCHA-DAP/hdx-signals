3cf1d571a7d9b79b5cace452d0364d26dc54af06
Seth Caldwell <caldwellst@gmail.com>
2024-07-09 16:13:56 +0100

    Adjust breaks

--- src/images/plots/breaks_date.R
+++ src/images/plots/breaks_date.R
@@ -3,8 +3,8 @@ box::use(scales)
 
 #' Generate breaks for date axis
 #'
-#' From the date axis, checks the number of years covered. If 3 or more years
-#' are included in the data, then the breaks are set to be the years. Otherwise,
+#' From the date axis, checks the number of years covered. If 4 to 10 distinct
+#' years are included in the data, then the breaks are set to be the years. Otherwise,
 #' `scales::breaks_pretty()` is used. We check the max compared to min because
 #' it is not always the raw vector passed in, but sometimes the limits of the
 #' function.
@@ -15,8 +15,8 @@ box::use(scales)
 breaks_date <- function(x) {
   max_year <- lubridate$year(max(x))
   min_year <- lubridate$year(min(x))
-  n_years <- max_year - min_year
-  if (n_years >= 2) {
+  years_diff <- max_year - min_year
+  if (years_diff >= 3 & years_diff <= 9) {
     scales$breaks_width(width = "1 year")(x)
   } else {
     scales$breaks_pretty()(x)
