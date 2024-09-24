eeb6c57a5a4a9aefad8e67b9ef67007f9851575a
Seth Caldwell <caldwellst@gmail.com>
2024-09-24 15:26:25 +0100

    Fix IDMC

--- docs/datasets/internal-displacement.md
+++ docs/datasets/internal-displacement.md
@@ -10,6 +10,8 @@ We monitor the [Internal Displacement Monitoring Centre's (IDMC) ](https://www.i
 
 We first remove duplicated event databased on event ID. If multiple entries for a single event ID, then only the recommended figures are kept if available. If unavailable, then the latest data updated into the IDU is kept based on the `created_at` date. We then transform this IDMC event data into a time series and calculate monthly displacements as the rolling sum of displacements across the past 30 days.
 
+For HRP countries and conflict-driven displacement, we only consider the time series to start from the first reported displacement event. This is because the IDMC IDU has uncertain temporal coverage. This temporary fix will be removed once the IDMC provides metadata indicating the start date for coverage for each country.
+
 Signals are generated whenever monthly displacements reach levels not seen in the past one or three years. Given the significant differences in conflict and natural disaster displacement, signals are generated separately for each type. Signals are not generated if monthly fatalities are below the minimum threshold of 5,000 (conflict-driven) and 50,000 (disaster-driven).
 
 #### Examples
--- src/indicators/idmc_displacement/utils/wrangle_displacement.R
+++ src/indicators/idmc_displacement/utils/wrangle_displacement.R
@@ -4,6 +4,10 @@ box::use(
   zoo
 )
 
+box::use(
+  src/utils/add_locations_metadata
+)
+
 #' Wrangle displacement data
 #'
 #' Takes in the IDMC displacement data and turns it into the proper format for generating
@@ -13,19 +17,22 @@ box::use(
 #'     displacement and other types of displacement.
 #' * Generates 7 day and 30 day rolling sums for plotting and alerting respectively.
 #'
+#' If the dataset is on conflict, the IDMC IDU currently has issues where only
+#' very recent years, maybe 2022 or 2023 onward, has reliable data for certain countries.
+#' To fix this, we currently filter the data for any HRP country to drop the beginning
+#' of the time series if there is no reported displacement. This should be removed
+#' once the IDMC is able to fix their system to match more what ACLED is able to
+#' provide.
+#'
 #' @param df_raw Raw conflict data frame
 #'
 #' @returns Wrangled data frame
 #'
 #' @export
 wrangle <- function(df_raw) {
-  df_raw |>
-    dplyr$mutate(
-      displacement_type = ifelse(
-        displacement_type == "Conflict",
-        "Conflict",
-        "Disaster"
-      )
+  df_wrangled <- df_raw |>
+    dplyr$filter(
+      displacement_type %in% c("Conflict", "Disaster") # other is no longer used by IDMC
     ) |>
     dplyr$group_by(
       displacement_type, iso3
@@ -47,4 +54,25 @@ wrangle <- function(df_raw) {
       displacement_30d
     ) |>
     dplyr$ungroup()
+
+  # temporary filtering to fix IDMC issues
+  if (unique(df_wrangled$displacement_type) == "Conflict") {
+    df_wrangled <- df_wrangled |>
+      add_locations_metadata$add_locations_metadata() |>
+      dplyr$group_by(
+        iso3
+      ) |>
+      dplyr$filter(
+        cumsum(dplyr$displacement_daily) > 0 | !hrp # only do the filtering for HRP countries
+      ) |>
+      dplyr$ungroup() |>
+      dplyr$select(
+        iso3,
+        displacement_type,
+        date,
+        displacement_daily,
+        displacement_7d,
+        displacement_30d
+      )
+  }
 }
