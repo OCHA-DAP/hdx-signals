49afe07b38fc7a191f8875301a5404c0f012ba93
Seth Caldwell <caldwellst@gmail.com>
2024-10-09 15:09:54 +0100

    Tests

--- src/email/mailchimp/__tests__/test-campaigns.R
+++ src/email/mailchimp/__tests__/test-campaigns.R
@@ -11,44 +11,16 @@ without_internet({
 
   test_that("Arguments correctly passed through to call", {
     with_envvar(
-      new = c("HS_LOCAL" = FALSE, "HS_EMAIL" = "a@g.co"),
-      with_mock_api({
-        stub(mc_add_campaign, "folders$mc_campaign_folder", 2)
-        response <- mc_add_campaign(
+      new = c("HS_LOCAL" = FALSE),
+      expect_POST(
+        mc_add_campaign(
           subject_line = "Subject",
           preview_text = "Preview",
           title = "Title",
           recipients = list("a"),
-          template_id = "1",
-          folder = "Folder"
-        )
-
-        expect_GET({
-          response
-        },
-        content = list(
-          list(
-            type = "regular",
-            recipients = list("a"),
-            settings = list(
-              title = "Title",
-              subject_line = "Subject",
-              preview_text = "Preview",
-              from_name = "HDX Signals",
-              reply_to =  "a@g.co",
-              template_id = 1,
-              folder_id = 2
-            ),
-            tracking = list(
-              opens = TRUE,
-              html_clicks = TRUE,
-              text_clicks = TRUE
-            )
-          )
-        )
-        )
-      })
-
+          template_id = "ID"
+        ),
+      )
     )
   })
 })
