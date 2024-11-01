a4cbe66f8b0025dae218af16fa1b23d55847a3a7
Seth Caldwell <caldwellst@gmail.com>
2024-11-01 23:18:51 +0000

    Add env var

--- src/email/mailchimp/__tests__/test-campaigns.R
+++ src/email/mailchimp/__tests__/test-campaigns.R
@@ -1,6 +1,6 @@
 test_that("mc_add_campaign works correctly with local TRUE", {
   with_envvar(
-    new = c(HS_LOCAL = TRUE),{
+    new = c(HS_LOCAL = TRUE, MAILCHIMP_API_KEY = "ABC"),{
       result <- mc_add_campaign(
         subject_line = "Subject",
         preview_text = "Preview",
@@ -17,7 +17,7 @@ test_that("mc_add_campaign works correctly with local TRUE", {
 test_that("mc_add_campaign works correctly with local FALSE", {
   with_mock_api({
     with_envvar(
-      new = c(HS_LOCAL = FALSE, HS_EMAIL = "test@example.com"),{
+      new = c(HS_LOCAL = FALSE, HS_EMAIL = "test@example.com", MAILCHIMP_API_KEY = "ABC"),{
         # Mock functions
         mock_mc_campaign_folder_id <- mock("1", cycle = TRUE)
         stub(mc_add_campaign, "folders$mc_campaign_folder_id", mock_mc_campaign_folder_id)
@@ -38,7 +38,7 @@ test_that("mc_add_campaign works correctly with local FALSE", {
 
 test_that("mc_send_campaign works correctly with local true", {
   without_internet({
-    with_envvar(new = c(HS_LOCAL = TRUE), {
+    with_envvar(new = c(HS_LOCAL = TRUE, MAILCHIMP_API_KEY = "ABC"), {
       result <- mc_send_campaign("campaign-id")
 
       expect_equal(result$method, "POST")
@@ -49,7 +49,7 @@ test_that("mc_send_campaign works correctly with local true", {
 
 test_that("mc_send_campaign works correctly with local FALSE", {
   without_internet({
-    with_envvar(new = c(HS_LOCAL = FALSE), {
+    with_envvar(new = c(HS_LOCAL = FALSE, MAILCHIMP_API_KEY = "ABC"), {
       expect_POST(
         mc_send_campaign("campaign-id"),
         "https://us14.api.mailchimp.com/3.0/campaigns/campaign-id/actions/send"
@@ -60,7 +60,7 @@ test_that("mc_send_campaign works correctly with local FALSE", {
 
 test_that("mc_campaign_info works correctly with local TRUE", {
   without_internet({
-    with_envvar(new = c(HS_LOCAL = TRUE), {
+    with_envvar(new = c(HS_LOCAL = TRUE, MAILCHIMP_API_KEY = "ABC"), {
       result <- mc_campaign_info("campaign-id")
 
       expect_equal(result$method, "GET")
@@ -71,7 +71,7 @@ test_that("mc_campaign_info works correctly with local TRUE", {
 
 test_that("mc_campaign_info works correctly with local FALSE", {
   without_internet({
-    with_envvar(new = c(HS_LOCAL = FALSE), {
+    with_envvar(new = c(HS_LOCAL = FALSE, MAILCHIMP_API_KEY = "ABC"), {
       expect_GET(
         mc_campaign_info("campaign-id"),
         "https://us14.api.mailchimp.com/3.0/campaigns/campaign-id"
