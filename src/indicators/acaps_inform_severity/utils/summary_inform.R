8cd90e4d59531d009a1d8592074bafafbc68ae5b
Giulia Martini <martini.giulia94@gmail.com>
2025-03-17 16:11:47 +0100

    Feat:add backup info for summary

--- src/indicators/acaps_inform_severity/utils/summary_inform.R
+++ src/indicators/acaps_inform_severity/utils/summary_inform.R
@@ -5,7 +5,6 @@ box::use(
   lubridate,
   purrr
 )
-
 box::use(
   src/utils/ai_summarizer,
   src/utils/get_prompts,
@@ -95,14 +94,15 @@ summary <- function(df_alerts, df_wrangled, df_raw) {
     ) |>
     purrr$list_rbind()
    df_info <-dplyr$bind_rows(list(df_info, df_event_info))
-
    df_event_risk <- httr2$request("https://api.acaps.org/api/v1/risk-list/") |>
      httr2$req_headers(
        Authorization = paste("Token", get_env$get_env("ACAPS_TOKEN"))
      ) |>
      httr2$req_url_query(
        iso3 = as.character(df_wrangled_alerting[ii, "iso3"]),
+       crisis_id = as.character(df_wrangled_alerting[ii, "crisis_id"]),
        `_internal_filter_date_gte` = as.character(format(df_wrangled_alerting[ii, "date"] - 365, "%Y-%m-%d")),
+       `_internal_filter_date_lte` = as.character(dplyr$pull(df_wrangled_alerting[ii, "date"]) + months(1)),
        .multi = "explode"
      )|>
      httr2$req_retry(
@@ -116,19 +116,36 @@ summary <- function(df_alerts, df_wrangled, df_raw) {
      ) |>
      purrr$map(
        .f = \(x) {
+         # Debug lengths
+         lengths <- sapply(x, function(col) if (is.null(col)) 0 else length(unlist(col)))
+         # Find maximum length to align columns
+         max_length <- max(lengths)
+
          dplyr$tibble(
-           iso3 = unlist(x$iso3),
-           risk_title = unlist(x$risk_title),
-           rationale = unlist(x$rationale),
-           vulnerability = unlist(x$vulnerability),
-           internal_date = as.Date(x$`_internal_filter_date`),
-           date =  dplyr$pull(df_wrangled_alerting[ii, "date"])
+           iso3 = rep(unlist(x$iso3), length.out = max_length),
+           risk_title = rep(unlist(x$risk_title), length.out = max_length),
+           rationale = rep(unlist(x$rationale), length.out = max_length),
+           vulnerability = rep(unlist(x$vulnerability), length.out = max_length),
+           crisis_id = rep(unlist(x$crisis_id), length.out = max_length),
+           internal_date = rep(as.Date(x$`_internal_filter_date`), length.out = max_length),
+           date = rep(dplyr$pull(df_wrangled_alerting[ii, "date"]), length.out = max_length)
          )
        }
-     ) |>
-     purrr$list_rbind()
+     )
+   df_event_risk <- if(length(df_event_risk) != 0){
+    purrr$list_rbind(df_event_risk)|>dplyr$filter(iso3==as.character(df_wrangled_alerting[ii, "iso3"]))
+   } else {
+     df_risk[0,]
+   }
+   df_risk <- dplyr$bind_rows(list(df_risk, df_event_risk))
   }
-  df_risk <- dplyr$bind_rows(list(df_risk, df_event_risk))
+
+  df_risk <- df_risk |>
+    dplyr$group_by(iso3, crisis_id) |>
+    dplyr$filter(internal_date==max(internal_date))
+
+  df_risk <- df_risk |> dplyr$group_by(iso3, crisis_id, date) |>
+    dplyr$summarize(text2 = paste(risk_title, rationale, vulnerability, sep=": ", collapse = ", "), .groups = "drop")
 
   df_info <- df_info |>
     dplyr$group_by(iso3, crisis_id, indicator) |>
@@ -143,6 +160,8 @@ summary <- function(df_alerts, df_wrangled, df_raw) {
       paste,
       c(dplyr$across(-all_of(id_cols)), sep = " ")
     )) |> dplyr$select(c('iso3', 'date', "crisis_id", "text"))
+
+  df_info <- df_info |> dplyr$full_join(df_risk, by=c("iso3", "crisis_id", "date")) |> dplyr$select(c('iso3', 'date', "text", "text2"))
 
   # now join together and get summarizations
   df_summary <- df_alerts |>
@@ -171,6 +190,36 @@ summary <- function(df_alerts, df_wrangled, df_raw) {
           ),
           .f = ai_summarizer$ai_summarizer_without_location
         )
+      ),
+      summary_source = "ACAPS reporting"
+    )|>
+    dplyr$mutate(
+      summary_long1 = purrr$map2_chr(
+        .x = prompts$long,
+        .y = text,
+        .f = ai_summarizer$ai_summarizer
+      ),
+      summary_long2 = purrr$map2_chr(
+        .x = prompts$long,
+        .y = text2,
+        .f = ai_summarizer$ai_summarizer
+      ),
+      summary_short = ifelse(
+        is.na(summary_long1) | summary_long1 == ""|is.na(summary_long2) | summary_long2 == "",
+        plot_title,
+        purrr$pmap_chr(
+          .l = list(
+            prompt = prompts$short,
+            info = paste(summary_long1, summary_long2, sep=","),
+            location = location
+          ),
+          .f = ai_summarizer$ai_summarizer_without_location
+        )
+      ),
+      summary_long = ifelse(
+        is.na(summary_long1) | summary_long1 == "",
+        summary_long2,
+        summary_long1
       ),
       summary_source = "ACAPS reporting"
     )
