dabfebf6ab41a4b6e708b8af943103ffce94caec
Giulia Martini <martini.giulia94@gmail.com>
2025-03-17 16:03:24 +0100

    Chore: clean code

--- src/indicators/acaps_inform_severity/utils/raw_inform.R
+++ src/indicators/acaps_inform_severity/utils/raw_inform.R
@@ -26,7 +26,7 @@ raw <- function() {
     df <- cs$read_az_file("output/acaps_inform_severity/raw.parquet")
     if (max(df$date) < as.Date(format(Sys.Date(), "%Y-%m-01"))) {
       df <- get_acaps_severity()
-      # upload full data frame to cloud
+    # upload full data frame to cloud
       cs$update_az_file(df, "output/acaps_inform_severity/raw.parquet")
     }
   } else {
@@ -88,7 +88,9 @@ get_acaps_severity_date <- function(formatted_date) {
           people_condition = x$`Conditions of affected people`,
           complexity = x$Complexity,
           drivers = paste(x$drivers, collapse = ", "),
-          date = as.Date(x$`_internal_filter_date`)
+          date = as.Date(x$`_internal_filter_date`),
+          reliability = x$reliability,
+          reliability_score = x$reliability_score,
         )
       }
     ) |>
@@ -136,46 +138,6 @@ get_acaps_severity_date <- function(formatted_date) {
   df_n_crises = df_all_crises|> dplyr$group_by(iso3, date) |> dplyr$summarise(count = dplyr$n_distinct(crisis_id), .groups = "drop")
 
   df_n_crises = df_all_crises|> dplyr$group_by(iso3, date) |> dplyr$summarise(count = dplyr$n_distinct(crisis_id), .groups = "drop")
-
-  # # Get risks list
-  # df_risks<-httr2$request("https://api.acaps.org/api/v1/risk-list/") |>
-  #   httr2$req_url_path_append(
-  #     formatted_date
-  #   ) |>
-  #   httr2$req_headers(
-  #     Authorization = paste("Token", get_env$get_env("ACAPS_TOKEN"))
-  #   ) |>
-  #   httr2$req_retry(
-  #     max_tries = 5,
-  #     backoff = \(x) x / 10
-  #   ) |>
-  #   httr2$req_error( # only return an error if not in the latest month
-  #     is_error = \(resp) if (format(Sys.Date(), "%b%Y") == formatted_date || !httr2$resp_is_error(resp)) FALSE else TRUE
-  #   ) |>
-  #   httr2$req_perform() |>
-  #   httr2$resp_body_json() |>
-  #   purrr$pluck(
-  #     "results"
-  #   ) |>
-  #   purrr$map(
-  #     .f = \(x) {
-  #       dplyr$tibble(
-  #         risk_id = x$risk_id,
-  #         risk_title = x$risk_title,
-  #         geographical_level = x$geographical_level,
-  #         risk_type = x$risk_type,
-  #         iso3 = unlist(x$iso3),
-  #         country = unlist(x$country),
-  #         crisis_id = x$crisis_id,
-  #         rationale = x$rationale,
-  #         trigger = x$trigger,
-  #         vulnerability = x$vulnerability,
-  #         date = as.Date(x$`_internal_filter_date`)
-  #       )
-  #     }
-  #   ) |>
-  #   purrr$list_rbind()
-
 
   df<- df_country_crises|>dplyr$left_join(df_n_crises, by=c('iso3', 'date'))
   df
