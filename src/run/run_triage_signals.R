c6a298340ee6917b37f0c2a7db458b24d99b363b
Giulia Martini <martini.giulia94@gmail.com>
2025-07-31 10:55:20 +0200

    Fix: import function in the correct way

--- src/run/run_triage_signals.R
+++ src/run/run_triage_signals.R
@@ -3,7 +3,7 @@ box::use(
   src/utils/get_env,
 )
 
-triage_signals(
+triage_signals$triage_signals(
   indicator_id = get_env$get_env("INDICATOR_ID"),
   n_campaigns = 0,
   test = get_env$get_env("TEST")
