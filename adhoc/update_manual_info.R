a02574df1ae48362e44fa8137a090384b81bac73
Davide Longo <davide.longo99@outlook.it>
2025-11-05 12:07:22 +0100

    adding new script to update manual info dataset

---
+++ adhoc/update_manual_info.R
@@ -0,0 +1,41 @@
+box::use(
+  src/utils/cloud_storage,
+  src/utils/read_manual_info,
+  src/utils/get_manual_info,
+  src/utils/validate_manual_info
+)
+
+
+new_row <- data.frame(
+  iso3 = "AFG",
+  indicator_id = "idmc_displacement_conflict",
+  date = as.Date("2025-11-05"),
+  text1 = "test",
+  text2 = "test",
+  stringsAsFactors = FALSE
+)
+
+# 1. Read existing file
+df_existing <- read_manual_info$read_manual_info()
+
+# 2. Add new row
+df_updated <- rbind(df_existing, new_row)
+
+# 3. Update file
+cloud_storage$update_az_file(
+  df = df_updated,
+  name = "input/manual_context_info.csv",
+  container = "dev"
+)
+
+### CHECKS ###
+
+#
+# df <- read_manual_info$read_manual_info()
+#
+# df_validated <- validate_manual_info$validate_manual_info(df)
+#
+# get_manual_info$get_manual_info("AFG", "idmc_displacement_conflict")
+
+###
+
