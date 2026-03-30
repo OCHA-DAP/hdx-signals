c0c2c4bbb60508b0e8494e860d28b4e53c01b57c
Giulia Martini <martini.giulia94@gmail.com>
2026-03-30 11:06:27 +0200

    fix: test

--- src/utils/__tests__/test-python_error_propagation.R
+++ src/utils/__tests__/test-python_error_propagation.R
@@ -5,20 +5,7 @@ box::use(
 impl <- attr(python_setup, "namespace")
 
 test_that("get_summary_r propagates python errors", {
-
   fn <- impl$get_summary_r
-
-  # Inject fake Python module
-  impl$reticulate <- list(
-    py = list(
-      get_summary = function(...) {
-        stop("Missing environment variables")
-      }
-    )
-  )
-
-  expect_error(
-    fn("sys", "user", "info"),
-    "Missing environment variables"
-  )
-})
+  mock_py_get_summary_r <- mock(stop("Missing environment variables"))
+  stub(fn, "reticulate::py$get_summary_r", mock_py_get_summary_r )
+  expect_error( fn("user"), "missing 1 required positional argument" )})
--- src/utils/__tests__/test-python_summarizer.R
+++ src/utils/__tests__/test-python_summarizer.R
@@ -10,9 +10,9 @@ test_that("get_summary_r returns mocked value", {
   fn <- impl$get_summary_r
 
   mock_py_get_summary <- mock("Result from Python")
-
+  Sys.setenv("HS_LOCAL"=FALSE)
   stub( fn, "reticulate::py$get_summary", mock_py_get_summary )
-  result <- fn( system_prompt = "sys", user_prompt = "user", info = "info", location = NULL )
+  result <- fn(system_prompt = "sys", user_prompt = "user", info = "info", location = NULL )
 
   expect_equal(result, "Result from Python")
   expect_called(mock_py_get_summary, n = 1)
